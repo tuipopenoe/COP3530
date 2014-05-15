97cf519fe6b0fc8ac917c3b7d748bd14a6d6784a
Tui Popenoe <tuipopenoe@gmail.com>
2014-05-15 11:21:46 -0500

    Assignment 1

---
+++ assignment_1/header.h
@@ -0,0 +1,12 @@
+// Tui Popenoe
+// COP 3530 Project 1
+// header.h
+// ©2014
+
+#include <iostream>
+
+using namespace std;
+
+void print_hello(){
+    cout << "Hello World!"<<endl;
+}
---
+++ assignment_1/main.cpp
@@ -0,0 +1,12 @@
+// Tui Popenoe
+// COP 3530 Project 1
+// main.cpp
+// ©2014
+
+#include "header.h"
+
+int main(){
+    print_hello();
+
+    return 0;
+}
